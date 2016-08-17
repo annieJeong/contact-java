d83b29facd08be7917ebea017fc1366fab69e686
kosta <kosta@kosta-HP>
2016-08-17 14:20:09 +0900

    fix

--- src/kr/co/kosta/contact/service/ContactService.java
+++ src/kr/co/kosta/contact/service/ContactService.java
@@ -10,24 +10,24 @@ import java.util.List;
  */
 public interface ContactService {
 	/**
-	 * �޼ҵ弳��
+	 * 연락처 추가하기.
 	 * @param contact
 	 * 
 	 */
 	void registContact(Contact contact);
 	/**
-	 * 
+	 * 연락처 전체보기
 	 * @return
 	 */
 	List<Contact> listContact();
 	/**
-	 * 
+	 * 연락처 상세보기
 	 * @param name
 	 * @return
 	 */
 	Contact detailContact(String name);
 	/**
-	 * 
+	 * 연락처 삭제
 	 * @param name
 	 */
 	void removeContact(String name);
